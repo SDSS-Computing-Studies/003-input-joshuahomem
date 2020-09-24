ceb07b8bb6ec0010500a0fe115d10dbc8969ef04
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-09-24 11:12:06 -0700

    Update task5.py

--- task5.py
+++ task5.py
@@ -13,3 +13,10 @@
 # Note: You will need to do some strange things with your cube root.
 # Remember that a cube root is the same as an exponent of 1/3, but
 # here you will need to do a power of 1.0/3 or something strange happens.
+volume=input("enter in volume ")
+volume=float(volume)
+import math
+value1=volume/(4/3)
+value2=value1/math.pi
+value3=value2**1/3
+print(value3)
